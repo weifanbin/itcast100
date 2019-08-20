7160c5af857e3ab1850d50e9b071f4388c5d89d8
wfb <blueboy.hi@sohu.com>
2019-08-20 11:08:10 +0800

    张三提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("李四写的代码");
+        System.out.println("张三写的代码");
     }
 }
