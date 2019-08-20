02843692a8a883c7926af4396d4a714a18d7c958
wfb <blueboy.hi@sohu.com>
2019-08-20 11:06:58 +0800

    修改内容

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("李四写的代码");
+        System.out.println("重新写的代码");
     }
 }
